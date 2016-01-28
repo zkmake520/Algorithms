913be3dd338f28e8edcfd933e285a2d769b8cb0c
kanzhou <kanzhou@usc.edu>
2016-01-28 14:44:20 -0800

    HappyNumber By using Floyed Cycle detection algorithm

---
+++ HappyNumberO_1.java
@@ -0,0 +1,20 @@
+public class HappyNumberO_1 {
+   public boolean isHappy(int n) {
+    	int fast = n, slow = n;
+        do{
+    		slow = getNewNum(slow);
+    		fast = getNewNum(fast);
+    		fast = getNewNum(fast);
+    	} while(fast != slow);
+        return slow==1?true:false;
+    }
+    int getNewNum(int n){
+        int sum = 0;
+        while(n != 0){
+            int digit =n % 10;
+            n = n/10;
+            sum += digit *digit;
+        }
+        return sum;
+    }
+}
